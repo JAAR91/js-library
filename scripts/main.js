f0982151ed73fc8f234f829258fcfdf8d1fcd54a
JAAR91 <jaarriagaramos@gmail.com>
2021-06-21 16:42:08 -0600

    fix issue with linters that does not recognize a functions correctly

--- scripts/main.js
+++ scripts/main.js
@@ -5,10 +5,10 @@ for (let i = 0; i < localStorage.length; i += 1) {
   myLibrary.push(JSON.parse(localStorage.getItem(key)));
 }
 
-const title = document.getElementById('inp_title');
-const author = document.getElementById('inp_author');
-const pages = document.getElementById('inp_pages');
-const description = document.getElementById('inp_description');
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const pages = document.getElementById('pages');
+const description = document.getElementById('description');
 const read = document.getElementById('inp_toggle');
 const submit = document.getElementById('inpsubmit');
 const bookContainer = document.getElementById('bookContainer');
@@ -21,46 +21,58 @@ function Book(title, author, pages, description, read) {
   this.read = read;
 }
 
-function addBookToLibrary() {
+submit.onclick = function addBookToLibrary() {
   const newBook = new Book(title.value, author.value, pages.value, description.value, read.checked);
   localStorage.setItem(title.value, JSON.stringify(newBook));
   window.location.reload();
-}
-
-submit.onclick = function () {
-  addBookToLibrary();
 };
 
-function deleteBook(title) {
-  localStorage.removeItem(title);
-  window.location.reload();
-}
-
-function updateToggle(title) {
-  const oBook = JSON.parse(localStorage.getItem(title));
-  oBook.read = read.checked;
-  localStorage.setItem(title, JSON.stringify(oBook));
-  window.location.reload();
-}
-
 function renderBook(book) {
-  let checked = '';
-  if (book.read === true) {
-    checked = 'checked';
-  }
-  const template = `<div class="card mb-3 me-3" style="width: 18rem;">
-    <div class="card-body" id="bookbody">
+  const card = document.createElement('div');
+  card.classList.add('card', 'mb-3', 'me-3');
+  card.style.width = '18rem';
+  bookContainer.appendChild(card);
+
+  const cardBody = document.createElement('div');
+  cardBody.classList.add('card-body');
+  card.appendChild(cardBody);
+
+  const template = `
         <h5 class="card-title">${book.title}</h5>
         <h6 class="card-subtitle mb-2 text-muted">${book.author}</h6>
         <p class="card-text">${book.description}</p>
-        <p class="card-text">${book.pages}<span>Pages</span></p>
-        <div class="form-check form-switch p-3 mt-3" role="group" aria-label="Basic radio toggle button group">
-          <input type="checkbox" class="form-check-input m-0" name="read" id="inp_toggle" autocomplete="off" value="false" ${checked} onclick="updateToggle('${book.title}')">
-          <label class="form-check-label m-0 mx-1" for="inp_toggle">Have you read this book? </label>
-        </div>
-        <button type="button" class="btn btn-danger text-white card-link" onclick="deleteBook('${book.title}')">Delete</button>
-    </div>
-  </div>`;
-  bookContainer.innerHTML += template;
+        <p class="card-text">${book.pages}<span>Pages</span></p>`;
+  cardBody.innerHTML += template;
+  const checkContainer = document.createElement('div');
+  checkContainer.classList.add('form-check', 'form-switch', 'p-3', 'mt-3');
+  cardBody.appendChild(checkContainer);
+
+  const checkBox = document.createElement('input');
+  checkBox.type = 'checkbox';
+  checkBox.checked = book.read;
+  checkBox.classList.add('form-check-input', 'm-0');
+  checkBox.onclick = function updateToggle() {
+    if (book.read === true) {
+      book.read = false;
+    } else {
+      book.read = true;
+    }
+    localStorage.setItem(book.title, JSON.stringify(book));
+  };
+  checkContainer.appendChild(checkBox);
+
+  const checklabel = document.createElement('label');
+  checklabel.classList.add('form-check-label', 'm-0', 'mx-1');
+  checklabel.textContent = 'I have read this book!';
+  checkContainer.appendChild(checklabel);
+
+  const deleteBtn = document.createElement('button');
+  deleteBtn.classList.add('btn', 'btn-danger', 'text-white', 'card-link');
+  deleteBtn.textContent = 'Delete';
+  deleteBtn.onclick = function deleteBook() {
+    localStorage.removeItem(book.title);
+    window.location.reload();
+  };
+  cardBody.appendChild(deleteBtn);
 }
 myLibrary.forEach((book) => renderBook(book));
