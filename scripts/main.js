1a5556cc347b014ad9642355a71751667a5c25f3
JAAR91 <jaarriagaramos@gmail.com>
2021-06-21 12:52:53 -0600

    add code for delete button

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,5 @@
+let myLibrary = [];
 
-
-const myLibrary = [
-    new Book('Americana', 'Chimanda', 300, 'A young girl in a foriegn land', false),
-    new Book('Born Rich', 'Rick Aderson', 234, 'make money while young', true),
-  ];
 for(let i=0;i<localStorage.length;i++){
     const key = localStorage.key(i);
     myLibrary.push(JSON.parse(localStorage.getItem(key)));
@@ -14,7 +10,7 @@ const inp_author = document.getElementById('inp_author');
 const inp_pages = document.getElementById('inp_pages');
 const inp_description = document.getElementById('inp_description');
 const inp_read = document.getElementById('inp_toggle');
-const submit = document.getElementById('inpsubmit');
+const inp_submit = document.getElementById('inpsubmit');
 const bookContainer = document.getElementById('bookContainer');
 
 function Book(title, author, pages, description, read ) {
@@ -26,32 +22,42 @@ function Book(title, author, pages, description, read ) {
 }
 
 function addBookToLibrary() {
-
     //myLibrary.push(new Book(inp_title.value, inp_author.value, inp_pages.value, inp_description.value, inp_read.checked));
-
     localStorage.setItem(inp_title.value, JSON.stringify(new Book(inp_title.value, inp_author.value, inp_pages.value, inp_description.value, inp_read.checked)));
     location.reload();
 }
 
+function deleteBook(title){
+  console.log(title);
+  localStorage.removeItem(title);
+  //location.reload();
+}
+
 function renderBook(book, ind) {
+    let checked = '';
+    if(book.read === true){
+      checked = 'checked';
+    }
     const template = `<div class="card mb-3 me-3" style="width: 18rem;">
       <div class="card-body">
           <h5 class="card-title">${book.title}</h5>
           <h6 class="card-subtitle mb-2 text-muted">${book.author}</h6>
           <p class="card-text">${book.description}</p>
           <p class="card-text">${book.pages}<span>Pages</span></p>
-          <button type="button" class="btn btn-info text-white card-link" 
-            id="inp_toggle-${ind}">${book.read ? 'Read' : 'Mark as read'}</button>
-          <button type="button" class="btn btn-danger text-white card-link" id="delete-${ind}">Delete</button>
+          <div class="form-check form-switch p-3 mt-3" role="group" aria-label="Basic radio toggle button group">
+            <input type="checkbox" class="form-check-input m-0" name="read" id="inp_toggle" autocomplete="off" value="false" ${checked}>
+            <label class="form-check-label m-0 mx-1" for="inp_toggle">Have you read this book? </label>
+          </div>
+          <button type="button" class="btn btn-danger text-white card-link" id="delete${ind}">Delete</button>
       </div>
     </div>`;
+
     bookContainer.innerHTML += template;
-    console.log(book.author);
+    document.getElementById('delete' + ind).onclick = function(){
+      console.log(book.title);
+      //deleteBook(book.title);
+    }
   }
 
-  function getBooks() {
-    bookContainer.innerHTML = '';
-    myLibrary.forEach((book, ind) => renderBook(book, ind));
-  }
+  myLibrary.forEach((book, ind) => renderBook(book, ind));
 
-  getBooks();
